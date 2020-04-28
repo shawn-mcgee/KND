c9e3b5599e45ac096ef7f8a2000595c8c9bf9d93
Shawn McGee <s.mcgeek07@gmail.com>
2020-04-28 16:42:02 -0600

    Initial commit

---
+++ knd-server/server.js
@@ -0,0 +1,7 @@
+const io = require('socket.io')(3000)
+
+io.on('connection', socket => {
+    socket.on('chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
+})
