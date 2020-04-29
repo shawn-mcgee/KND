44d0cc3fa14fb0481ab1c7913daa939967f0e9b1
Shawn McGee <s.mcgeek07@gmail.com>
2020-04-28 18:46:41 -0600

    0.0.1

--- knd-server/server.js
+++ knd-server/server.js
@@ -1,7 +1,21 @@
-const io = require('socket.io')(3000)
+const http = require('http')
+const fs   = require('fs'  )
 
-io.on('connection', socket => {
-    socket.on('chat-message', message => {
-        socket.broadcast.emit('chat-message', message)
+const port = 80
+const addr = '0.0.0.0'
+
+const server = http.createServer((req, res) => {
+    fs.readFile('../knd-client/index.html', (err, out) => {
+        if(err)
+            console.log(err)
+        else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(out)
+            res.end()
+        }
     })
+})
+
+server.listen(port, addr, () => {
+
 })
