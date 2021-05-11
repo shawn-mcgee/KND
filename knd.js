c89231bf39aa71c14c1b47d5f174274b4f63ca4a
shawn-mcgee <s.mcgeek07@gmail.com>
2021-05-10 18:35:53 -0600

    0.0.12

--- knd.js
+++ knd.js
@@ -2,8 +2,8 @@ const app = require('http').createServer(handle)
 const io  = require('socket.io')(app)
 const fs  = require('fs')
 
-const raw = fs.readFileSync('./server.json')
-const cfg = JSON.parse(raw)
+var raw = fs.readFileSync('./knd.json')
+var cfg = JSON.parse(raw)
 
 const port = (cfg.port ? cfg.port :         80)
 const addr = (cfg.addr ? cfg.addr :  '0.0.0.0')
@@ -12,39 +12,13 @@ const motd = (cfg.motd ? cfg.motd.trim() : 'Welcome to KND!')
 const root = './public'
 const home = '/client.html'
 
-let mode = (cfg.mode ? cfg.mode :           1);
-let mask = (cfg.mask ? cfg.mask : -2147483607);
-
-const clients = new Map();
-
-function uid32() {
-    let lsb = mode & 1;
-    mode >>= 1;
-    if(lsb == 1)
-        mode ^= mask;
-    return mode;
-}
-
 app.listen(port, addr, () => {
     console.log('Listening on /' + addr + ':' + port)
 })
 
-io.on('connect', (socket) => {
+io.on('connection', (socket) => {
     if(motd !== '')
         socket.emit('rx_message', motd)
-    socket.on('uid32', (_uid32) => {
-        if(!clients.has(_uid32)) {
-            console.log("invalid uid32 " + _uid32)
-            console.log(clients)
-
-            let new_uid32 = uid32()
-            clients.set(new_uid32, 1)        
-            socket.emit('uid32', new_uid32)
-            
-        } else {
-            socket.emit('uid32', _uid32)
-        }
-    })    
     socket.on('tx_message', (message) => {
         socket.          emit('tx_message', message)
         socket.broadcast.emit('rx_message', message)
